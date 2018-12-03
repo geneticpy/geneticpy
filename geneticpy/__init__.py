0c665c309f49e9da0109b67f33bb9e2a759cf251
Brandon Schabell <brandonschabell@gmail.com>
2018-12-02 21:32:22 -0600

    setting verbose to false by default

--- geneticpy/__init__.py
+++ geneticpy/__init__.py
@@ -3,7 +3,7 @@ from geneticpy.population import Population
 
 
 def optimize(fn, param_space, size=100, generation_count=10, percentage_to_randomly_spawn=0.05, mutate_chance=0.25,
-             retain_percentage=0.6, maximize_fn=False, verbose=True):
+             retain_percentage=0.6, maximize_fn=False, verbose=False):
     pop = Population(fn=fn, params=param_space, size=size, percentage_to_randomly_spawn=percentage_to_randomly_spawn,
                      mutate_chance=mutate_chance, retain_percentage=retain_percentage,
                      maximize_fn=maximize_fn, verbose=verbose)
