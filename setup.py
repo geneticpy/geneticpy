6106d755e48680be8742d41098a2ad3e1a2f5a2f
Brandon Schabell <brandonschabell@gmail.com>
2018-12-02 21:42:03 -0600

    updated setup

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     url='https://github.com/geneticpy/geneticpy',
+    download_url='https://github.com/geneticpy/geneticpy/archive/v0.0.1.tar.gz',
     license='MIT',
     author='Brandon Schabell',
     author_email='brandonschabell@gmail.com',
