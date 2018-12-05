c12cebe702e58a90866cc76ac02a45329e9a5228
Brandon Schabell <brandonschabell@gmail.com>
2018-12-05 17:09:29 -0600

    added time output to optimize()

--- geneticpy/__init__.py
+++ geneticpy/__init__.py
@@ -1,6 +1,7 @@
 from geneticpy.distributions import *
 from geneticpy.population import Population
 from tqdm import tqdm
+from time import time
 
 
 def optimize(fn, param_space, size=100, generation_count=10, percentage_to_randomly_spawn=0.05, mutate_chance=0.25,
@@ -11,6 +12,7 @@ def optimize(fn, param_space, size=100, generation_count=10, percentage_to_rando
     else:
         t = None
 
+    start_time = time()
     pop = Population(fn=fn, params=param_space, size=size, percentage_to_randomly_spawn=percentage_to_randomly_spawn,
                      mutate_chance=mutate_chance, retain_percentage=retain_percentage, maximize_fn=maximize_fn,
                      tqdm_obj=t)
@@ -21,6 +23,7 @@ def optimize(fn, param_space, size=100, generation_count=10, percentage_to_rando
 
     top_score = pop.get_top_score()
     top_params = pop.get_top_params()
+    total_time = time() - start_time
     if t is not None:
         t.close()
-    return top_params, top_score
+    return top_params, top_score, total_time
