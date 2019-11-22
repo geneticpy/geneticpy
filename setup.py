2e315e05a2723870c9279bd0beeee6f8c5e5a2fc
Brandon Schabell <brandonschabell@gmail.com>
2019-11-22 12:50:36 -0600

    incremented version to 1.1.0

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ test_requirements = [
 
 setup(
     name='geneticpy',
-    version='1.0.1',
+    version='1.1.0',
     packages=find_packages(),
     url='https://github.com/geneticpy/geneticpy',
-    download_url='https://github.com/geneticpy/geneticpy/archive/v1.0.1.tar.gz',
+    download_url='https://github.com/geneticpy/geneticpy/archive/v1.1.0.tar.gz',
     license='MIT',
     author='Brandon Schabell',
     author_email='brandonschabell@gmail.com',
